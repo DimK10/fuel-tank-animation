71da84a6f653f728c6c34fcafae9308296de32c6
Dimitris Kaitantzidis <james_k23@hotmail.gr>
2020-10-05 23:02:03 +0300

    Initial commit -- make more beautiful with colors

---
+++ magic.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $('.tk').each(function () {
+    var amount = $('.lq').attr('data-amount');
+    var quantity = amount;
+    /* if	(amount < 50) {
+    amount = parseInt(amount) + 10
+  } */
+    /* $('.lq').css({height : amount  + '%'}); */
+    $(this)
+      .find('.lq')
+      .animate({ height: parseInt(amount) + '%' }, 1000, function () {});
+    $('.ring').css({ height: 100 - amount + 10 + '%' });
+    $('.text').text(quantity + '%');
+  });
+  $('.text').each(function () {
+    var $this = $(this);
+    jQuery({ Counter: 0 }).animate(
+      { Counter: $this.text() },
+      {
+        duration: 1000,
+        easing: 'swing',
+        step: function () {
+          $this.text(Math.ceil(this.Counter) + '%');
+        },
+      }
+    );
+  });
+});
